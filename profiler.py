aaabd9fcdd99f188f9b220c4bbc01b9b13159ec7
Otacon22 <danieleiamartino@gmail.com>
2014-01-26 19:27:17 +0100

    script for profiling the performance

---
+++ profiler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import os
+
+fd = open("performance_data.csv","w")
+
+for workitems in [1,16,32,64,128,256,512,1024,2048,4096]:
+    for loops in [1,16,32,64,128,256,512,1024,2048,4096,10000]:
+        print "Executing: "+str(workitems)+","+str(loops)
+        out = os.popen("./main -c -w "+str(workitems)+" -b "+str(loops)).read().split("\n")[1]
+        fd.write(out+"\n")
+        fd.flush()
