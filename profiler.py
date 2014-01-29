ec2f09a99c292f4cedf67ded7676dae4c12b0098
Otacon22 <danieleiamartino@gmail.com>
2014-01-29 23:08:25 +0100

    more meaningful values and drop not useful output

--- profiler.py
+++ profiler.py
@@ -4,11 +4,12 @@ from time import time
 
 fd = open("performance_data"+str(int(time()))+".csv","w")
 
-for workitems in [32,64,128,256,512,1024,2048,4096,8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]:
+for workitems in [256,512,1024,2048,4096,8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]:
     for loops in [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,10000,15000,20000]:
-	for wgsize in [1,2,4,8,16,32,64,128,256,512,1024]:
+	for wgsize in [32,64,128,256]:
 	    print "Executing: "+str(workitems)+","+str(loops)
             cmd = "./main -c -w "+str(workitems)+" -b "+str(loops)+" -g "+str(wgsize)
             out = os.popen(cmd).read().split("\n")[1]
-            fd.write(out+"|"+cmd+"\n")
+            if len(out.split(","))==8:
+                fd.write(out+"|"+cmd+"\n")
             fd.flush()
