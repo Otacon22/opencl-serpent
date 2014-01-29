f2e584c0215ef92e3be5cec27269f8ffb9871b76
Otacon22 <danieleiamartino@gmail.com>
2014-01-29 20:40:24 +0100

    added some parameters to profiler

--- profiler.py
+++ profiler.py
@@ -4,7 +4,7 @@ from time import time
 
 fd = open("performance_data"+str(int(time()))+".csv","w")
 
-for workitems in [1,16,32,64,128,256,512,1024,2048,4096]:
+for workitems in [64,128,256,512,1024,2048,4096]:
     for loops in [1,16,32,64,128,256,512,1024,2048,4096,10000]:
         print "Executing: "+str(workitems)+","+str(loops)
         out = os.popen("./main -c -w "+str(workitems)+" -b "+str(loops)).read().split("\n")[1]
