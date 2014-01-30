2be5ad9490859cc1bc5e5d51cb4c9dccc495c6ed
Otacon22 <danieleiamartino@gmail.com>
2014-01-30 01:51:53 +0100

    Added iteration for avarage calculation purposes

--- profiler.py
+++ profiler.py
@@ -6,10 +6,12 @@ fd = open("performance_data"+str(int(time()))+".csv","w")
 
 for workitems in [256,512,1024,2048,4096,8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]:
     for loops in [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,10000,15000,20000]:
-	for wgsize in [32,64,128,256]:
-	    print "Executing: "+str(workitems)+","+str(loops)
-            cmd = "./main -c -w "+str(workitems)+" -b "+str(loops)+" -g "+str(wgsize)
-            out = os.popen(cmd).read().split("\n")[1]
-            if len(out.split(","))==8:
-                fd.write(out+"|"+cmd+"\n")
-            fd.flush()
+        for wgsize in [32,64,128,256]:
+            for x in range(3): # For getting mean values
+                print "Executing: "+str(workitems)+","+str(loops)
+                cmd = "./main -c -w "+str(workitems)+" -b "+str(loops)+" -g "+str(wgsize)
+                out = os.popen(cmd).read().split("\n")[1]
+                if len(out.split(","))==8:
+                    fd.write(out+"|"+cmd+"\n")
+                fd.flush()
+
